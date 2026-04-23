078b53d2514695543d7ce65e040c978051d0ad69
RodrigoSuar <Rodrigo.suarez3004@gmail.com>
2026-04-23 00:34:10 -0500

    production build update

--- backend/src/services/s3.js
+++ backend/src/services/s3.js
@@ -1,12 +1,22 @@
 const { S3Client } = require("@aws-sdk/client-s3");
 const config = require("../utils/config.js");
 
+
+//for development
+// const s3 = new S3Client({
+//     region: config.AWS_REGION,
+//     credentials: {
+//         accessKeyId: config.AWS_ACCESS_KEY,
+//         secretAccessKey: config.AWS_SECRET_KEY,
+//     },
+// });
+
+
+//for production
 const s3 = new S3Client({
     region: config.AWS_REGION,
-    credentials: {
-        accessKeyId: config.AWS_ACCESS_KEY,
-        secretAccessKey: config.AWS_SECRET_KEY,
-    },
+    
 });
+
 
 module.exports = s3;
